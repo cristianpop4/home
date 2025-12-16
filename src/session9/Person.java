0b443c959de63dcba0ec7632e9ee4099186205d8
cristianpop4 <ionutcristianpop44@gmail.com>
2025-12-16 20:39:05 +0200

    session9

---
+++ src/session9/Person.java
@@ -0,0 +1,33 @@
+package session9;
+
+public class Person {
+
+    private String name;
+    private int age;
+
+    public Person(String name , int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setAge(int age){
+        this.age = age;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    public void getDetails(){
+        System.out.println("Nuemle persoanei: " + name);
+        System.out.println("Varsta persoanei: " + age);
+    }
+}
